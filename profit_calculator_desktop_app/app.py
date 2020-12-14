561af62240e354ebdde98c133eea5c404a7e48a3
akaimar <39114801+akaimar@users.noreply.github.com>
2020-12-14 22:16:37 +0200

    Update app.py

--- profit_calculator_desktop_app/app.py
+++ profit_calculator_desktop_app/app.py
@@ -4,23 +4,28 @@ from fbs_runtime.application_context.PyQt5 import ApplicationContext
 import finviz
 from datetime import datetime
 
-#app .exe file is compled by using fbs # pip install fbs
-#app new project C:\Users\aimar.roosalu\AppData\Roaming\Python\Python37\Scripts\ ### fbs startproject
-#app main.py location: C:\Users\aimar.roosalu\AppData\Roaming\Python\Python37\Scripts\target\MyApp
-#app compile C:\Users\aimar.roosalu\AppData\Roaming\Python\Python37\Scripts\ ### fbs freeze to complie to .exe
-#produced by Aimar Roosalu 04/2020 aimar.roosalu@gmail.com
+#KASUTATUD DOKUMENTATSIOON:
+#https://www.riverbankcomputing.com/static/Docs/PyQt5/index.html
+#Finviz API dokumentatsioon: https://github.com/mariostoev/finviz
 
+#JUHIS KUIDAS GENEREERIDA antud python koodist .EXE laiendiga Windosi süsteemidele töötav rakendus
+#app .exe file kompileeritakse kasutades fbs moodulit - fbs # pip install fbs
+#app vali õige path projekti jaoks C:\Users\aimar.roosalu\AppData\Roaming\Python\Python37\Scripts\ ### fbs startproject
+#app main.py location: C:\Users\aimar.roosalu\AppData\Roaming\Python\Python37\Scripts\target\MyApp
+#app kompileerime C:\Users\aimar.roosalu\AppData\Roaming\Python\Python37\Scripts\ ### fbs freeze et kompileerida .exe fail
+#selle koodi autor on Aimar Roosalu 11/2020 aimar.roosalu@gmail.com
+
+#erinevate widgetite koordinaadid
 hor = 10
-ver = 50
 hor1 = hor+70
 
-class Window(QtWidgets.QMainWindow):
+class Window(QtWidgets.QMainWindow): #akna konteiner
     
     def __init__(self):
         super(Window, self).__init__()
-        self.setGeometry(100, 100, 300, 550)
+        self.setGeometry(200, 200, 300, 550)
         self.setWindowTitle("Kasumi kalkulaator")
-        self.setWindowIcon(QtGui.QIcon('icon.png')) # tuli importida QtGui
+        self.setWindowIcon(QtGui.QIcon('icon.png')) # tuli importida QtGui, et saada ikoon
         
         self.home()
 
@@ -61,45 +66,45 @@ class Window(QtWidgets.QMainWindow):
         
  
         # <!---- WIDGETID ----->
-        self.ticker = QtWidgets.QLineEdit(self)
+        self.ticker = QtWidgets.QLineEdit("", self)
         self.ticker.resize(100, 20)
-        self.ticker.move(80, 30)
-        self.ticker.editingFinished.connect(self.aktsiahind)
+        self.ticker.move(100, 30)
+        self.ticker.editingFinished.connect(self.aktsiahind) #Kui siit kastist väljuda, käivitub funktsioon "aktsiahind"
  
         self.ostuhind = QtWidgets.QDoubleSpinBox(self)
         self.ostuhind.setSingleStep(0.01)
         self.ostuhind.setMaximum(99999)
         self.ostuhind.resize(100, 20)
-        self.ostuhind.move(80, 55)
+        self.ostuhind.move(100, 55)
         
         self.ostukogus = QtWidgets.QSpinBox(self)
         self.ostukogus.resize(100, 20)
-        self.ostukogus.move(80, 80)
+        self.ostukogus.move(100, 80)
         
         self.myygihind = QtWidgets.QDoubleSpinBox(self)
         self.myygihind.setSingleStep(0.01)
         self.myygihind.setMaximum(99999)
         self.myygihind.resize(100, 20)
-        self.myygihind.move(80, 105)
+        self.myygihind.move(100, 105)
         
         self.myygikogus = QtWidgets.QSpinBox(self)
         self.myygikogus.resize(100, 20)
-        self.myygikogus.move(80, 130)
+        self.myygikogus.move(100, 130)
         
         self.ostukulu = QtWidgets.QDoubleSpinBox(self)
         self.ostukulu.setSingleStep(0.01)
         self.ostukulu.resize(100, 20)
-        self.ostukulu.move(80, 155)
+        self.ostukulu.move(100, 155)
 
         self.myygikulu = QtWidgets.QDoubleSpinBox(self)
         self.myygikulu.setSingleStep(0.01)
         self.myygikulu.resize(100, 20)
-        self.myygikulu.move(80, 180)
+        self.myygikulu.move(100, 180)
         
         # Button
         btn = QtWidgets.QPushButton("Arvuta", self)
         btn.resize(50, 25)
-        btn.move(80, 210)
+        btn.move(100, 210)
         btn.clicked.connect(self.arvuta)
         
         # INFOTAHVLI VÄLJUNDID
@@ -109,49 +114,58 @@ class Window(QtWidgets.QMainWindow):
         self.print_0l.move(10, 275)
         
         self.print_aktsiahind = QtWidgets.QLabel(self)
-        self.print_aktsiahind.move(hor+hor1, 275)
+        self.print_aktsiahind.setGeometry(QtCore.QRect(hor+hor1, 285, 200, 10))
 
         self.print_1l = QtWidgets.QLabel("Kogukulu: ", self)
         self.print_1l.move(10, 300)
 
         self.print_kogukulu = QtWidgets.QLabel(self)
-        self.print_kogukulu.move(hor+hor1, 50+245)
+        self.print_kogukulu.move(hor+hor1, 50+250)
 
         self.print_2l = QtWidgets.QLabel("Kasum: ", self)
         self.print_2l.move(10, 325)
         
         self.print_kasum = QtWidgets.QLabel(self)
-        self.print_kasum.move(hor+hor1, 50+270)
+        self.print_kasum.move(hor+hor1, 50+275)
         
         self.print_3l = QtWidgets.QLabel("Tehingukulu: ", self)
         self.print_3l.move(10, 350)
         
         self.print_kulu = QtWidgets.QLabel(self)
-        self.print_kulu.move(hor+hor1, 50+295)
+        self.print_kulu.move(hor+hor1, 50+300)
         
         self.print_4l = QtWidgets.QLabel("Breakeven: ", self)
         self.print_4l.move(10, 375)
         
         self.print_breakeven = QtWidgets.QLabel(self)
-        self.print_breakeven.move(hor+hor1, 50+320)
+        self.print_breakeven.move(hor+hor1, 50+325)
     
         
         #COPYRIGHT        
-        self.print_copyright = QtWidgets.QLabel("(C) 2020 A.R.", self)
-        self.print_copyright.move(10, 50+400)
-    
+        self.print_copyright = QtWidgets.QLabel("__________________________________\nCopyright 2020 A.R.\nSisesta aktsia lühend (nt. AAPL).\nHind tuleb turult automaatselt ja \non 15 min. viitajaga.", self)
+        self.print_copyright.setGeometry(QtCore.QRect(10, 450, 300, 100))
         self.show()
 
-#FUNKTSIOONID aka MEETODID
-        
+#FUNKTSIOONID
+         
     # finviz api kaudu viimane hind (15 min delay)
     def aktsiahind(self):
-        if self.ticker.text() == "": #validaator, et väli ei oleks tühi ja errori vältimiseks
-            self.print_aktsiahind.setText("Sisesta ticker")
-        else:    
-            stock = finviz.get_stock(self.ticker.text())
-            self.ostuhind.setValue(float(stock['Price'])) #prindime välja ostuhinna väljale, tuleb konverteerida komakohtadeks
-            self.print_aktsiahind.setText(str(stock['Price'])) #prindime ka alla infotahvlile
+
+        #valideerimine - lubatud sümbolid tickeri jaoks
+        lubatud = {'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z'}
+
+        kontroll_string = self.ticker.text()
+        if lubatud.issuperset(kontroll_string) == True: #kui sisestatud sümbol vastab tingimustele, peab sisaldama ainult lubatud tähti
+            
+            try: 
+                stock = finviz.get_stock(self.ticker.text())
+                self.ostuhind.setValue(float(stock['Price'])) #prindime välja ostuhinna väljale, tuleb konverteerida komakohtadeks
+                self.print_aktsiahind.setText(str(stock['Price'])) #prindime ka alla infotahvlile
+            except:
+                self.print_aktsiahind.setText(str("Sellist aktsiat ei ole!")) #prindime ka alla infotahvlile
+        
+        else:
+            self.print_aktsiahind.setText(str("Lubatud ainult tähed!")) #prindime ka alla infotahvlile
 
     # arvutused infotahvlile
     def arvuta(self):
@@ -160,7 +174,7 @@ class Window(QtWidgets.QMainWindow):
         c = self.myygihind.value()
         d = self.myygikogus.value()
         e = self.ostukulu.value()
-        f = self.myygikulu.value()            
+        f = self.myygikulu.value()
         
         #arvutused ja ümardame kahe komakohani 
         kogukulu = round(a * b, 2)
@@ -181,11 +195,12 @@ class Window(QtWidgets.QMainWindow):
 #mainloop
 def run():
     app = QtWidgets.QApplication(sys.argv)
-    app.setStyle(QtWidgets.QStyleFactory.create('fusion'))
+    app.setStyle(QtWidgets.QStyleFactory.create('fusion')) #akna stiiliks on "fusion"
     GUI = Window()
     app.exec_()
 
 run()
+
 
 
 
